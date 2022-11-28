4371c9486e6da7788188e2a5570fc9036ba1bf23
Andrea-Baggio <andreabaggio800@gmail.com>
2022-11-28 15:14:53 +0100

    css senza le cards

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <MainPageVue />
+</template>
+
+<script>
+import MainPageVue from './components/MainPage.vue';
+
+export default {
+  components: {
+    MainPageVue,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: rgb(35, 35, 35);
+}
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1>
+      Spotify
+    </h1>
+    <div class="content">
+      <div class="content-box">
+        <AlbumsPageVue />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AlbumsPageVue from './AlbumsPage.vue';
+
+export default {
+  name: 'MainPage',
+  components: {
+    AlbumsPageVue,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+  color: rgb(194, 194, 194);
+  background-color: rgb(49, 49, 49);
+  padding: .6rem 0;
+}
+</style>
